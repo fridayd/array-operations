02ca351cab4f94ecf3b808bff220c3d70c2da861
Ben Dudson <bd512@york.ac.uk>
2018-03-06 23:01:37 +0000

    find-array-dimensions working
    Function which finds AREF and ROW-MAJOR-AREF calls,
    returning an alist of symbol to expressions which calculate
    the index range.
    
    FOREACH macro currently just calls find-array-dimensions,
    looking up dimension expressions:
    
        * (foreach i :do (aref a 0 i))
        I -> (ARRAY-DIMENSION A 1)
    
        * (foreach (i j) :do (* (aref A i j) 2.0))
        I -> (ARRAY-DIMENSION A 0)
        J -> (ARRAY-DIMENSION A 1)
    
    Catches some basic errors: index not a symbol, and array dimensions
    not determined.

--- src/foreach.lisp
+++ src/foreach.lisp
@@ -1,13 +1,13 @@
 (in-package #:array-operations)
 
-(defun walk-array-indices (expr)
-  "Walks an expression tree EXPR, finds AREF calls. 
-   Returns a list of (symbol, array, index).
+(defun find-array-dimensions (expr)
+  "Walks an expression tree EXPR, finds AREF and ROW-MAJOR-AREF calls. 
+   Returns a list of (symbol, expr)
 
    Example: 
-     (walk-array-indices '(+ (aref a i) (* 2 (aref b j k))))
+     (find-array-dimensions '(+ (aref a i) (* 2 (aref b j k))))
    
-   -> ((I A . 0) (K B . 1) (J B . 0))
+   -> ((I ARRAY-DIMENSION A 0) (K ARRAY-DIMENSION B 1) (J ARRAY-DIMENSION B 0))
     
   "
   (cond
@@ -22,13 +22,27 @@
             (symlist (cddr expr) (cdr symlist))
             ;; Build list of index constraints (symbol, array, index)
             (result nil (if (symbolp (first symlist))
-                            (cons (cons (first symlist) (cons arr ind)) result)
+                            (cons (list (first symlist) 'array-dimension arr ind) result)
                             result)))
            ((not symlist) result))))
+
+    ;; If EXPR is ROW-MAJOR-AREF
+    ((equalp (first expr) 'row-major-aref)
+     (list (list (third expr) 'array-total-size (second expr))))
     
     ;; Otherwise, walk elements of the list
     ;; join together the alist
-    (t (mapcan #'walk-array-indices expr))))
+    (t (mapcan #'find-array-dimensions expr))))
 
-(defmacro foreach (syms &key (sum nil) &body body)
-  
+(defmacro foreach (syms &key (sum nil) ((:do body) nil))
+  (let* ((dim-exprs (find-array-dimensions body))
+         (syms (if (listp syms) syms
+                   (list syms)))) ; Ensure that syms is a list
+    (dolist (sym syms)
+      ;; Check that SYM is a symbol
+      (unless (symbolp sym) (error "Index must be a symbol ~S" sym))
+      
+      ;; Find an expression which sets the range of SYM
+      (let ((dim-expr (assoc sym dim-exprs)))
+        (unless dim-expr (error "Cannot determine range of index ~S" sym))
+        (format t "~a -> ~a~%" sym (rest dim-expr))))))
